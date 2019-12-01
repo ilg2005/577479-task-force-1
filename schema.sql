388001726ad87a62b362b6ffca67b4fb35083047
ilg2005 <igoldfarb10@gmail.com>
2019-12-01 12:50:28 +0300

    таблица users

--- schema.sql
+++ schema.sql
@@ -3,3 +3,19 @@ CREATE DATABASE taskforce
     DEFAULT COLLATE utf8_general_ci;
 
 USE taskforce;
+
+CREATE TABLE users
+(
+    id                int AUTO_INCREMENT PRIMARY KEY,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email             VARCHAR(128) NOT NULL,
+    password          VARCHAR(128) NOT NULL,
+    role              int DEFAULT 0
+);
+
+create unique index users_email_uindex
+    on users (email);
+create index users_registration_date_index
+    on users (registration_date);
+create index users_role_index
+    on users (role);
