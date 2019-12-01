fc809f408d6e53402a5793c59721761051ff2ac3
ilg2005 <igoldfarb10@gmail.com>
2019-12-01 16:06:33 +0300

    текущее состояние

--- schema.sql
+++ schema.sql
@@ -4,18 +4,30 @@ CREATE DATABASE taskforce
 
 USE taskforce;
 
+CREATE TABLE profile
+(
+    id int AUTO_INCREMENT PRIMARY KEY
+);
+
 CREATE TABLE users
 (
     id                int AUTO_INCREMENT PRIMARY KEY,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name              VARCHAR(64)  NOT NULL,
     email             VARCHAR(128) NOT NULL,
     password          VARCHAR(128) NOT NULL,
-    role              int DEFAULT 0
+    role              int       DEFAULT 0,
+    profile_id        int,
+    FOREIGN KEY (profile_id) REFERENCES profile (id) ON DELETE CASCADE
 );
 
+create index users_name_index
+    on users (name);
 create unique index users_email_uindex
     on users (email);
 create index users_registration_date_index
     on users (registration_date);
 create index users_role_index
     on users (role);
+
+
