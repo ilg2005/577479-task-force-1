187732a0fa55bacacfe5019d1bbfb17f2933f613
ilg2005 <igoldfarb10@gmail.com>
2019-12-02 16:57:26 +0300

    таблица статистики пользователя

--- schema.sql
+++ schema.sql
@@ -44,10 +44,30 @@ CREATE TABLE users
     email                VARCHAR(128) NOT NULL,
     password             VARCHAR(128) NOT NULL,
     role                 TINYINT(1) DEFAULT 0,
-    rating               FLOAT      DEFAULT 0,
     latest_activity_time TIMESTAMP,
     profile_id           int,
     FOREIGN KEY (profile_id) REFERENCES profile (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_statistics
+(
+    user_id       int,
+    rating        FLOAT DEFAULT 0,
+    reviews_count INT   DEFAULT 0,
+    tasks_count   INT   DEFAULT 0,
+    views_count   INT   DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_settings
+(
+    user_id          int,
+    new_message      TINYINT DEFAULT 1,
+    actions_on_task  TINYINT DEFAULT 0,
+    new_review       TINYINT DEFAULT 0,
+    show_to_customer TINYINT DEFAULT 1,
+    hide_profile     TINYINT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 create index users_name_index
@@ -58,15 +78,9 @@ create index users_registration_date_index
     on users (registration_date);
 create index users_role_index
     on users (role);
-
-
-CREATE TABLE user_settings
-(
-    user_id int,
-    new_message  TINYINT DEFAULT 1,
-    actions_on_task TINYINT DEFAULT 0,
-    new_review TINYINT DEFAULT 0,
-    show_to_customer TINYINT DEFAULT 1,
-    hide_profile TINYINT DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
+create index user_rating_index
+    on user_statistics (rating);
+create index user_tasks_count_index
+    on user_statistics (tasks_count);
+create index user_views_count_index
+    on user_statistics (views_count);
